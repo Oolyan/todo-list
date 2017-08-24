9f5c992c43671f87fb40252dc32fca061c094b2d
Olga Nesterovskaya <o.nesterovska@gmail.com>
2017-08-24 22:02:56 +0200

    Change list view to table view

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 $(function(){
     var items = [];
 
-    function addItem() {
-        var $itemName = $('.add-item').val();
-        var item = {};
-        item.name = $itemName;
+    function addItem() {        
+        var item = {
+            name:  $('.name').val(),
+            surname: $('.surname').val(),
+            phone: $('.phone').val()
+        };
         items.push(item);
-        $('.todo-list').append('<li>' + $itemName + '</li>');
+
+        var tableItem = $('<tr>')
+            .append($('<td>').append(item.name))
+            .append($('<td>').append(item.surname))
+            .append($('<td>').append(item.phone));
+        $('.todo-list').append(tableItem);
+
+        // Row below is for testing proposes
         $('.test').html(JSON.stringify(items));
     };
-    
+
     $('.add-item-button').click( addItem );
     
 });
