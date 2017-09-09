f1739f9c531a3b9c2edfcd7aab38cd15a851f975
Olga Nesterovskaya <o.nesterovska@gmail.com>
2017-09-10 01:11:03 +0200

    Start to realize delete and edit functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function(){
     var items = [];
 
-    function addItem() {        
+    function addItem() {  
         var item = {
             name:  $('.name').val(),
             surname: $('.surname').val(),
@@ -12,13 +12,27 @@ $(function(){
         var tableItem = $('<tr>')
             .append($('<td>').append(item.name))
             .append($('<td>').append(item.surname))
-            .append($('<td>').append(item.phone));
-        $('.todo-list').append(tableItem);
+            .append($('<td>').append(item.phone))
+            .append($('<td>').append($('<button class = "edit-item-button">').append('EDIT')))
+            .append($('<td>').append($('<button class = "delete-item-button">').append('DELETE')));
+        $('.todo-list tbody').append(tableItem);
 
         // Row below is for testing proposes
-        $('.test').html(JSON.stringify(items));
+        //$('.test').html(JSON.stringify(items));
     };
 
     $('.add-item-button').click( addItem );
+
+    function deleteItem() {
+        $(this).closest('tr').remove();
+    };
+
+    function editItem() {
+        $(this).closest('td').siblings().css( "background-color", "red" );
+    };
+
+
+    $(document).on('click','.delete-item-button', deleteItem );
+    $(document).on('click','.edit-item-button', editItem );
     
 });
